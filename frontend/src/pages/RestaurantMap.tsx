5c72a06933357aad8ceda04986137bb3d3cefe85
Osama Sado <osama.sadow@gmail.com>
2026-09-07 15:27:51 +0200

    delete css file and its usage

--- frontend/src/pages/RestaurantMap.tsx
+++ frontend/src/pages/RestaurantMap.tsx
@@ -13,7 +13,6 @@ import {useEffect, useState} from "react";
 import {getRoute} from "../service/RouteService.ts";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "./RestaurantMap.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
